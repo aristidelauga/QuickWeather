15b13ba49d425d116fd6df0cd4e9edaa06d2cb5a
aristidelauga <aristide.lauga@gmail.com>
2023-02-14 09:24:54 +0100

    Initial Commit

---
+++ WeatherExomind/WeatherExomindApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherExomindApp.swift
+//  WeatherExomind
+//
+//  Created by Aristide LAUGA on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherExomindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
